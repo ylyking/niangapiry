90c5bcf89efa7856b276097843e78fba33afdef0
carlixyz <carlixyz@yahoo.com>
2012-07-10 21:39:15 +0000

    Finished TiledReader.js Script: - Select a file and clic on the Utility/TiledReader menu , or press the Ctrl + 't' Shortcut - set the tile size output and press "A Comeeerla" ,wait a few minutes, and voila! u got your tiled file - You can flip vertical, Horizontal and set rotation. - you can mix different Tilesets and materials in each Base or Layer ( Just disable the CombineMesh component in case of multiple materials ). - You can Add a single Tile Property for Collision just set inside Tiled in each tile a property called "Collision" . (It's extendable to a Layer or another properties)

--- Source/Assets/Scripts/Editor/TestingScript.js
+++ Source/Assets/Scripts/Editor/TestingScript.js
@@ -1,7 +1,6 @@
 
 @MenuItem("Utility / Test Script #_t") // "Ctrl + Shift + l"
 
-
 static function CreatePrefab() 
 { 
 	var FlippedHorizontallyFlag : int = 0x80000000;
@@ -20,8 +19,9 @@ static function CreatePrefab()
 	if (TileId & FlippedDiagonallyFlag)		
 		 print ( "Rotated") ;
 			 	 
-	var Samples : cTileSet = new cTileSet();
-	print ( Samples.SrcImgName + Samples.ModuleWidth );
+//	var Samples : cTileSet = new cTileSet();
+//	print ( Samples.SrcImgName + Samples.ModuleWidth );
 			
  print ("testing: " + TileId + " is " + Index );
 }
+
